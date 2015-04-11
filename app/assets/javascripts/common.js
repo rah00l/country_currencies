0ba95076a42bdb30480f412a834617349c7b7000
Rahul Patil <rahulpatil2387@gmail.com>
2015-04-11 23:05:07 +0530

    Modidyed js code and resolved search issue

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,35 +1,23 @@
-/* 
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+ $(function ()
+ {
+ 	$('#currencies_search input').keyup(function () {
+ 		$.get($('#currencies_search').attr('action'), $('#currencies_search').serialize(), null, 'script');
+ 		return false;
+ 	});
 
-//$(document).ready(function (){
-//     $('#new_review').submit(function (){
+ 	$('#countries_search input').keyup(function () {
+ 		$.get($('#countries_search').attr('action'), $('#countries_search').serialize(), null, 'script');
+ 		return false;
+ 	});
 
-$(function () {
-
-    $('#countries_search').keyup(function() {
-        $('#countries_search').submit();
-    });
-
-    $('#currencies_search').keyup(function() {
-        $('#currencies_search').submit();
-    });
-
-    $('#country_form').bind('ajax:complete', function(event, data) {
-        $('body, html').animate({
-            scrollTop:$('body').offset().top
-            }, 'slow');
-        alert("Successfully updated");
-    });
-
-    $("#currencies_search").submit(function (){
-        $.get(this.action, $(this).serialize(), null, 'script');
-        return false;
-    });
-
-    $("#countries_search").submit(function (){
-        $.get(this.action, $(this).serialize(), null, 'script');
-        return false;
-    });
-});
+ 	$('#country_form').bind('ajax:complete', function(event, data) {
+ 		$('body, html').animate({
+ 			scrollTop:$('body').offset().top
+ 		}, 'slow');
+      alert("Successfully updated");
+  });
+ });
