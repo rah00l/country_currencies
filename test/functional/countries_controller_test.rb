c7e05f1b0cf86a403ca2e4d50b5d1ac1277eba79
Rahul Patil <rahulpatil2387@gmail.com>
2015-05-10 13:01:03 +0530

    Modifie test cases syntax with latest syntax of should / context

--- test/functional/countries_controller_test.rb
+++ test/functional/countries_controller_test.rb
@@ -3,18 +3,25 @@ require 'test_helper'
 class CountriesControllerTest < ActionController::TestCase
   should_not_respond_to_actions :new => :get, :destroy => :get
 
+  include Devise::TestHelpers
+  # context "CountriesController" do
+
   setup do
-    @country = countries(:one)
+    # @country = countries(:one)
     @controller = CountriesController.new
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = FactoryGirl.create(:user)
+    @country = FactoryGirl.create(:country)
+    sign_in @user
   end
 
-  test "should get index" do
+  should "get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:countries)
   end
 
-  test "should create country" do
+  should "should create country" do
     assert_difference('Country.count') do
       post :create, :country => @country.attributes.merge({ :code => Time.now.to_s })
     end
@@ -22,7 +29,7 @@ class CountriesControllerTest < ActionController::TestCase
     assert_redirected_to country_path(assigns(:country))
   end
 
-  test "should not create duplicate currency" do
+  should "Not create duplicate currency" do
     assert_no_difference('Currency.count') do
       post :create, :country => @country.attributes
     end
@@ -30,17 +37,17 @@ class CountriesControllerTest < ActionController::TestCase
     assert !assigns[:country].errors[:code].empty?
   end
 
-  test "should show country" do
+  should "show country" do
     get :show, :id => @country.to_param
     assert_response :success
   end
 
-  test "should get edit" do
+  should "get edit" do
     get :edit, :id => @country.to_param
     assert_response :success
   end
 
-  test "should update country" do
+  should "able to update country" do
     put :update, :id => @country.to_param, :country => @country.attributes
     assert_redirected_to country_path(assigns(:country))
   end
