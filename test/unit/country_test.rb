72683539c8d5c97fe153f6b0435aaf8e7bde4e2a
Rahul Patil <rahulpatil2387@gmail.com>
2015-05-11 00:45:43 +0530

    Added test cases for country Model

--- test/unit/country_test.rb
+++ test/unit/country_test.rb
@@ -2,4 +2,17 @@ require 'test_helper'
 
 class CountryTest < ActiveSupport::TestCase
   test_validates_presence_of :name, :code
+
+  context "Country" do
+  	setup do
+  		@country = FactoryGirl.create(:country)
+  	end
+
+  	should "get records by search" do
+  		@country.stubs(:name).retuns("India")
+  		assert Country.search("ind")
+  		assert_equal "India",@country.name
+  	end
+  	
+  end
 end
