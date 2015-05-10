cc7ba3b3352a87dbee4c3186d2f649fcc585ff0c
Rahul Patil <rahulpatil2387@gmail.com>
2015-05-10 13:02:00 +0530

    Added configurations required for test environment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,38 @@
+require 'simplecov'
+require 'simplecov-json'
+# SimpleCov.start
+
+# SimpleCov report will only be generated when tests are executed like :
+# COVERAGE=true rake test
+SimpleCov.start 'rails' do
+
+  SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter,
+                          SimpleCov::Formatter::JSONFormatter ]
+end if ENV["COVERAGE"]
+
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# require 'minitest/unit'
+# require "minitest/rails"
+# require 'minitest/autorun'
+# require "minitest"
+# require 'minitest-spec-rails'
+# require "minitest/autorun"
+
+# require "minitest/reporters"
+# Minitest::Reporters.use!
+
+
+
+
+# To add Capybara feature tests add `gem "minitest-rails-capybara"`
+# to the test group in the Gemfile and uncomment the following:
+# require "minitest/rails/capybara"
+
+# Uncomment for awesome colorful output
+# require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -9,6 +41,7 @@ class ActiveSupport::TestCase
   # -- they do not yet inherit this setting
   fixtures :all
 
+  # Add more helper methods to be used by all tests here...
   def self.should_not_respond_to_actions(actions = {})
     actions.each do |name, method|
       test "should not respond to #{method.to_s.upcase} :#{name}" do
