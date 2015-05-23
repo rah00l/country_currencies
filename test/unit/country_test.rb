ab64f88e83e237e6fed31262f6c595fc72115a46
Rahul Patil <rahulpatil2387@gmail.com>
2015-05-23 22:08:45 +0530

    Indented code for CountryTest file

--- test/unit/country_test.rb
+++ test/unit/country_test.rb
@@ -1,18 +1,19 @@
 require 'test_helper'
 
 class CountryTest < ActiveSupport::TestCase
-  test_validates_presence_of :name, :code
-
+  # test_validates_presence_of :name, :code
   context "Country" do
-  	setup do
-  		@country = FactoryGirl.create(:country)
-  	end
+    setup do
+      @country = FactoryGirl.create(:country)
+    end
 
-  	should "get records by search" do
-  		@country.stubs(:name).returns("India")
-  		assert Country.search("ind")
-  		assert_equal "India",@country.name
-  	end
-  	
+    should "get records by search" do
+      # @country.stubs(:name).returns("India")
+
+      @country.stub :name, "India" do
+        assert Country.search("ind")
+        assert_equal "India",@country.name
+      end
+    end
   end
 end
